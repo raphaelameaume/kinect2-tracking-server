cafce443424c4ee899b0bcf93d23079a4b3b8d0b
Raphaël Améaume <raphael.ameaume@gmail.com>
2019-01-30 01:24:42 -0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# kinect2-tracking-server
+Tiny server to emit Kinect data with socket.io
+
+WIP
---
+++ examples/server.js
@@ -0,0 +1,4 @@
+var KinectServer = require('../index.js');
+
+var server = new KinectServer();
+server.listen();
